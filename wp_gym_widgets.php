4af384b35d6edde273f79f735ab7b0df5075b02f
Eric Whitcomb <eric.whitcomb@gmail.com>
2021-08-21 11:43:43 -0600

    Display WP_Gym_Classes_Widget if quantity and $post_count greater than 0

--- wp_gym_widgets.php
+++ wp_gym_widgets.php
@@ -34,46 +34,51 @@ class WP_Gym_Classes_Widget extends WP_Widget {
 	 * @param array $instance Saved values from database.
 	 */
 	public function widget( $args, $instance ) {
-		echo $args['before_widget']; ?>
+		$post_count = wp_count_posts('wp_gym_classes')->publish;
+		if ($instance['quantity'] > 0 && $post_count > 0):
+			echo $args['before_widget']; ?>
 
-		<h3 class="text-primary text-center sidebar-class-list-header">Our Classes</h3>
+			<h3 class="text-primary text-center sidebar-class-list-header">
+				<?php echo esc_html($instance['title']); ?>
+			</h3>
 
-		<ul class="sidebar-class-list">
-			<?php
-				$args = array(
-					'post_type' => 'wp_gym_classes',
-					'posts_per_page' => 3,
-					'orderby' => 'rand'
-				);
+			<ul class="sidebar-class-list">
+				<?php
+					$args = array(
+						'post_type' => 'wp_gym_classes',
+						'posts_per_page' => $instance['quantity'],
+						'orderby' => 'rand'
+					);
 
-				// Use WP_Query and append the results into $classes
-				$classes = new WP_Query($args);
-				while ($classes->have_posts()): $classes->the_post();
-			?>
+					// Use WP_Query and append the results into $classes
+					$classes = new WP_Query($args);
+					while ($classes->have_posts()): $classes->the_post();
+				?>
 
-			<li class="sidebar-class-item">
-				<div class="sidebar-class-image">
-					<?php the_post_thumbnail('thumbnail'); ?>
-				</div>
+				<li class="sidebar-class-item">
+					<div class="sidebar-class-image">
+						<?php the_post_thumbnail('thumbnail'); ?>
+					</div>
 
-				<div class="sidebar-class-content">
-					<a href="<?php the_permalink(); ?>">
-						<h4><?php the_title(); ?></h4>
-					</a>
+					<div class="sidebar-class-content">
+						<a href="<?php the_permalink(); ?>">
+							<h4><?php the_title(); ?></h4>
+						</a>
 
-					<?php 
-            $class_days = get_field('class_days');
-            $start_time = get_field('start_time');
-            $end_time = get_field('end_time');
-          ?>
-          <p><?php echo $class_days . '<br>' . $start_time . ' to ' . $end_time;?></p>
-				</div>
-			</li>
+						<?php 
+							$class_days = get_field('class_days');
+							$start_time = get_field('start_time');
+							$end_time = get_field('end_time');
+						?>
+						<p><?php echo $class_days . '<br>' . $start_time . ' to ' . $end_time;?></p>
+					</div>
+				</li>
 
-			<?php endwhile; wp_reset_postdata(); ?>
-		</ul>
-		
-		<?php echo $args['after_widget'];
+				<?php endwhile; wp_reset_postdata(); ?>
+			</ul>
+			
+			<?php echo $args['after_widget'];
+		endif;
 	}
 
 	/**
